8089f8ab78865ec8e4b3e512587f2718f9dcfe0c
Melissa Astbury <melissaastbury@hotmail.com>
2020-11-08 16:58:48 +0000

    grid now renders on the page. If the number is '1' the box will appear red

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let canvas = document.getElementById('myCanvas');
+let ctx = canvas.getContext('2d');
+
 function make2DArray(columns, rows) {
   let arr = new Array(columns);
 
@@ -8,18 +11,42 @@ function make2DArray(columns, rows) {
 }
 
 let grid;
-let columns = 10;
-let rows = 10;
+let columns;
+let rows;
+let resolution = 40;
+let width = canvas.width;
+let height = canvas.height;
 
 function setUp() {
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, 400, 400);
+
+  columns = width / resolution;
+  rows = height / resolution;
+
   grid = make2DArray(columns, rows);
 
   for (let i = 0; i < columns; i++) {
     for (let j = 0; j < rows; j++) {
-      grid[i][j] = Math.round(Math.random(2));
+      grid[i][j] = Math.round(Math.random());
+    }
+  }
+  draw();
+}
+
+function draw() {
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      let x = i * resolution;
+      let y = j * resolution;
+      if (grid[i][j] === 1) {
+        // Draw a rectangle
+        ctx.fillStyle = 'red';
+        ctx.fillRect(x, y, resolution, resolution);
+      }
+      ctx.strokeStyle = 'red';
+      ctx.strokeRect(x, y, resolution, resolution);
     }
   }
 }
-
 setUp();
-console.table(grid);
